bf28946267cd30196a24af2900ab1c387ad93d41
hairunisha23 <151384594+hairunisha23@users.noreply.github.com>
2023-12-31 14:58:24 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,50 @@
+package tugas9;
+
+import java.util.Date;
+
+public class Biodata {
+    String nama;
+
+    String alamat;
+
+    Date tanggalLahir;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
